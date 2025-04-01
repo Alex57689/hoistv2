67b069fd4009fdbc120d6df7b3e4eb8a4f877648
alex57689 <alexcostello20@gmai.com>
2025-04-01 14:20:14 +1000

    fix image links

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Banner: React.FC = () => {
   return (
-    <div className="relative w-full h-[500px] bg-[url('/IMG-0069.jpg')] bg-cover bg-center">
+    <div className="relative w-full h-[500px] bg-[url('images/IMG-0069.jpg')] bg-cover bg-center">
       <div className="absolute inset-0 bg-black/50 flex flex-col justify-center items-center">
         <h1 className="text-6xl md:text-8xl mb-4 font-bold text-white">Hoist Hydraulics</h1>
         <h2 className="text-2xl md:text-4xl mt-2 text-white">Explore Our Range</h2>
